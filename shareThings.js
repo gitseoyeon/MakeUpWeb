087ec72450f3bf6bcf4d2bb19ae6d9e5f0d10e6d
gitseoyeon <tjdus553926@gmail.com>
2023-11-10 23:19:08 +0900

    이전 주문 정보 확인

--- shareThings.js
+++ shareThings.js
@@ -37,6 +37,6 @@ function createPost(authorName, profileImageUrl, title, content, imageUrl) {
 }
 
 // 예시 게시물 추가
-createPost("홍길동", "images/userImage.png", "나만의 뷰티템을 갖고싶다면?!", "Make Your Beauty를 이용해보세요! 당신의 아름다움을 책임집니다!", "images/image1.png");
-createPost("토끼끼", "images/userImage2.png", "이것은 두 번째 게시물입니다.", "example2.jpg");
-createPost("코덕", "images/userImage3.png", "이것은 세 번째 게시물입니다.", "example3.jpg");
+createPost("관리자", "images/userImage.png", "나만의 뷰티템을 갖고싶다면?!", "Make Your Beauty를 이용해보세요! 당신의 아름다움을 책임집니다!", "images/image1.png");
+createPost("토끼끼", "images/userImage2.png", "안녕하세요~ 처음 글 써봅니다!", "화알못이라서 많이 부족하지만 배워가겠습니다~");
+createPost("코덕", "images/userImage3.png", "제 뷰티템 소개할게요!", "example3.jpg");
